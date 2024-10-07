938ac8307e671144647a712404bce5c21ca3f021
gomezzero <gomezjuanzero@gmail.com>
2024-10-07 07:06:26 -0500

    feat: create MyDbContex and conextion in database, create conection JWT and folder Confg, add class in Employee

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,12 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PruebaNET_JuanPabloGomezBustamante.Models
 {
+    [Table("employees")]
     public class Employee
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("identification_numeber")]
+        public string IdentificationNumeber { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
     }
 }
